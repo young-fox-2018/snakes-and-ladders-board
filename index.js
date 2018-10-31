994fcbe9b3642153860b48372168d58593443187
imanuel vicky <imanuvi@imanuels-MBP.localdomain>
2018-10-31 14:53:41 +0700

    menambahkan if else sehingga kalau input ganjil benar

--- index.js
+++ index.js
@@ -7,16 +7,27 @@ function generateBoard(dimension) {
         let temp = []
         let tempCount = num + 1
         for (let j = dimension; j > 0; j--) {
-            if (i % 2 == 0) {
-                temp.push(tempCount-j)
-            }else {
-                temp.push(num)
+            if (dimension % 2 == 0) { // genap
+                if (i % 2 == 0) {
+                    temp.push(tempCount-j)
+                }else {
+                    temp.push(num)
+                }
+                num -= 1
+            } else { // ganjil
+                if (i % 2 != 0) {
+                    temp.push(tempCount-j)
+                }else {
+                    temp.push(num)
+                }
+                num -= 1
             }
-            num -= 1
+
         }
         output.push(temp)
     }
     return output
 }
 
+//console.log(generateBoard(15))
 console.log(generateBoard(10))
