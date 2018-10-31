0f2ba120083f8c7d9f3d2465cb7b5c20be3e90df
anharfathoni <anharaf28@gmail.com>
2018-10-31 17:02:38 +0700

    update index.js snakes&ladders board

--- index.js
+++ index.js
@@ -1,33 +1,79 @@
 function generateBoard(dimensi){
-    var result = []
-    let initNumber = 1;
-    
-    for(let i = 0; i < dimensi; i++){
-        var arrayRow = []
-        for(let j = 0; j < dimensi; j++){
-            arrayRow.push(initNumber)
-            initNumber++
-            
-        }
-        // result.unshift(arrayRow)
-        if(i % 2 !== 0){
-            arrayRow = rev(arrayRow)
-        }
+    var initNumber = dimensi * dimensi
+    let result = []
 
-        result.push(arrayRow)
+    if(dimensi % 2 === 0){
+        // console.log("dimensi genap")
+        for(let i = dimensi; i > 0; i--){
+            let arrayRow = []
+            for(let j = dimensi; j > 0; j--){
+                if(i % 2 === 0){
+                    arrayRow.push(initNumber)
+                    initNumber--
+                }
+                else{
+                    arrayRow.push((i)*dimensi - (j-1))
+                    initNumber--
+                }
+            }
+            result.push(arrayRow)
+        }
     }
-    result = rev(result)
+    else{
+        // console.log("dimensi ganjil")
+        // console.log("init", initNumber)
+        for(let i = dimensi; i > 0; i--){
+            let arrayRow = []
+            var temp = initNumber
+            // console.log(i+"-"+temp)
+            // console.log("initNUm:",initNumber)
+            for(let j = dimensi; j > 0; j--){
+                if(i % 2 !== 0){
+                    arrayRow.push(temp - (j-1))
+                    initNumber -= 1
+                }
+                else{
+                    arrayRow.push(initNumber)
+                    temp = temp - 1
+                    initNumber -=1
+                }
+            }
+            result.push(arrayRow)
+        }
+    }
     return result
 }
 
-function rev(arr){
-    var reversed = []
-    for(let i = arr.length-1; i >= 0; i--){
-        reversed.push(arr[i])
-    }
-    return reversed
-}
 
 console.log(generateBoard(8))
 console.log("")
 console.log(generateBoard(15))
+
+// function generateBoard(dimensi){
+//     var result = []
+//     let initNumber = 1;
+    
+//     for(let i = 0; i < dimensi; i++){
+//         var arrayRow = []
+//         for(let j = 0; j < dimensi; j++){
+//             arrayRow.push(initNumber)
+//             initNumber++
+//         }
+//         // result.unshift(arrayRow)
+//         if(i % 2 !== 0){
+//             arrayRow = rev(arrayRow)
+//         }
+
+//         result.push(arrayRow)
+//     }
+//     result = rev(result)
+//     return result
+// }
+
+// function rev(arr){
+//     var reversed = []
+//     for(let i = arr.length-1; i >= 0; i--){
+//         reversed.push(arr[i])
+//     }
+//     return reversed
+// }
