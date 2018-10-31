2660405e5cfadc1536188038b464d53793c4d576
anharfathoni <anharaf28@gmail.com>
2018-10-31 15:23:57 +0700

    update index.js snake&ladders

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ function generateBoard(dimensi){
     for(let i = 0; i < dimensi; i++){
         var arrayRow = []
         for(let j = 0; j < dimensi; j++){
-            if(i % 2 === 0){
-                arrayRow.push(initNumber)
-                initNumber++
-            }
-            else{
-                arrayRow.unshift(initNumber)
-                initNumber++
-            }
+            arrayRow.push(initNumber)
+            initNumber++
+            
         }
-        result.unshift(arrayRow)
+        // result.unshift(arrayRow)
+        if(i % 2 !== 0){
+            arrayRow = rev(arrayRow)
+        }
+
+        result.push(arrayRow)
     }
+    result = rev(result)
     return result
+}
+
+function rev(arr){
+    var reversed = []
+    for(let i = arr.length-1; i >= 0; i--){
+        reversed.push(arr[i])
+    }
+    return reversed
 }
 
 console.log(generateBoard(8))
